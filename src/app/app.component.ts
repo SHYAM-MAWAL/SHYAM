7c21a363e294e718d8c61b009126aecf6b7a4c85
SHYAMMAWAL <shyammawal123@gmail.com>
2018-11-30 19:15:37 +0530

    LIST DEMO MOCK DATABASE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SHYAM MAWAL';
-
-  msg="";
-  nCnt: number=0;
-  clickMe(){
-    this.nCnt = this.nCnt + 1;
-    this.msg ="clicked" + this.nCnt;
-
-  }
-  
+  title = 'Tour of Heroes';
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. SHYAM' },
+  { id: 12, name: 'MANISH' },
+  { id: 13, name: 'SATISH' },
+  { id: 14, name: 'GAVRAO' },
+  { id: 15, name: 'SACHIN' },
+  { id: 16, name: 'SUVRANA' },
+  { id: 17, name: 'MEGHA' },
+  { id: 18, name: 'RUPALI' },
+  { id: 19, name: 'BALAJI' },
+  { id: 20, name: 'SUHAS' }
+];

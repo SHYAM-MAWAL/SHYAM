67e795e42e3822ea0ad071d51b5928c7db4e6b91
SHYAMMAWAL <shyammawal123@gmail.com>
2018-11-30 20:06:58 +0530

    Update Database Name

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -4,7 +4,7 @@ export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. SHYAM' },
   { id: 12, name: 'MANISH' },
   { id: 13, name: 'SATISH' },
-  { id: 14, name: 'GAVRAO' },
+  { id: 14, name: 'GAURAV' },
   { id: 15, name: 'SACHIN' },
   { id: 16, name: 'SUVRANA' },
   { id: 17, name: 'MEGHA' },
